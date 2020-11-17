e979716119e47f1372303f503507ce070d5828f1
Jiankun YANG <JYang53@slb.com>
2020-11-17 14:55:03 +0800

    feat: add AC2 feature

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,10 +2,17 @@ export class MultiplicationTable {
   private isStartSmaller(start:number, end: number): boolean {
     return start >= end ? true : false
   }
+  private isInputValidRange(start: number, end: number): boolean {
+    return (start >= 1 && start <= 10) && (end >= 1 && end <= 10)
+  }
   public render(start: number, end: number): string{
     if (this.isStartSmaller(start, end))
     {
       return ''
+    }
+    if(!this.isInputValidRange(start,end))
+    {
+      return 'input number is in invalid range'
     }
     return '1*1=1'
   }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -14,6 +14,16 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe('')
   })
 
+  it ('input should be outside range', () => {
+    //given
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 6
 
+    //when
+    const rendered = table.render(start,end)
 
+    //then
+    expect(rendered).toBe('input number is in invalid range')
+  })
 })
