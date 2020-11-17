b1e5b3fec17c36271f74a836fff1d8893718b7ad
Jiankun YANG <JYang53@slb.com>
2020-11-17 14:39:59 +0800

    feat: add return nothing feature

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,22 @@
 export class MultiplicationTable {
+  private isStartSmaller(start:number, end: number): boolean {
+    return start >= end ? true : false
+  }
   public render(start: number, end: number): string{
+    if (this.isStartSmaller(start, end))
+    {
+      return ''
+    }
     return '1*1=1'
   }
 }
+// function isInputValid (isSmaller: boolean) : string {
+//   if (isSmaller)
+//   {
+//     return ''
+//   }
+//   else
+//   {
+//     return 'valid input'
+//   }
+// }
