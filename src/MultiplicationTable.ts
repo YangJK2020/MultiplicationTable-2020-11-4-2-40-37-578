711edfa7bdb042e779aa00474f2c5fb5bb2636aa
Jiankun YANG <JYang53@slb.com>
2020-11-17 18:23:33 +0800

    feat: realize the 2-4 case

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -5,6 +5,28 @@ export class MultiplicationTable {
   private isInputValidRange(start: number, end: number): boolean {
     return (start >= 1 && start <= 10) && (end >= 1 && end <= 10)
   }
+
+  private calculateMultiplicationTable(start: number, end: number): Expression[][] {
+    const size = end - start
+    const result : Expression[][] = []
+    for (let i = 0; i <= size; ++i)
+    {
+      result[i]=[]
+      for(let j = 0; j <= i; ++j)
+      {
+        result[i].push(new Expression(start+j, start +i))
+      }
+    }
+    return result
+  }
+
+  private getOutPutString (input:Expression[][]): string{
+    let outPut = ''
+    outPut = input.map(row => row.map(expression => `${expression.factor1}*${expression.factor2}=${expression.product}`)
+      .join('  ')).join('\n')
+    return outPut
+  }
+
   public render(start: number, end: number): string{
     if (this.isStartSmaller(start, end))
     {
@@ -14,16 +36,18 @@ export class MultiplicationTable {
     {
       return 'input number is in invalid range'
     }
-    return '1*1=1'
+    const table = this.calculateMultiplicationTable(start,end)
+    const result = this.getOutPutString(table)
+    return result
   }
 }
-// function isInputValid (isSmaller: boolean) : string {
-//   if (isSmaller)
-//   {
-//     return ''
-//   }
-//   else
-//   {
-//     return 'valid input'
-//   }
-// }
+export class Expression {
+  factor1: number
+  factor2: number
+  product: number
+  constructor(start1:number, end1: number){
+    this.factor1 = start1
+    this.factor2 = end1
+    this.product = this.factor1 * this.factor2
+  }
+}
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -26,4 +26,19 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('input number is in invalid range')
   })
+
+  it ('test input 2,4', () => {
+    //given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    //when
+    const rendered = table.render(start,end)
+
+    //then
+    expect(rendered).toBe(`2*2=4
+2*3=6  3*3=9
+2*4=8  3*4=12  4*4=16`)
+  })
 })
